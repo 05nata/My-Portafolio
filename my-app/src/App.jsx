f1ed561009fa0bd1894c3a334ce54a6966ef39f9
Natalia Marin Gallego <0502.nataliia@gmail.com>
2022-03-15 00:23:10 -0500

    Create and route NotFound page

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import MyProjects from './components/pages/MyProjects';
+import NotFound from "./components/pages/NotFound";
 
 /**
  * App component
@@ -14,6 +15,7 @@ export const App = () => (
        <Route path='/' element={<Home />} />
        <Route path='/about' element={<About />} />
        <Route path='/Projects/:id' element={<MyProjects />} />
+       <Route path='*' element={<NotFound />} />
    </Routes>
    </BrowserRouter> 
 );
---
+++ my-app/src/components/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import DefaultLayout from "../layout/DefaultLayout";
+
+/**
+ * Not found component
+ * @returns {JSX.Element}
+ */
+export const NotFound = () => (
+    <DefaultLayout>
+        <h1>Not Found Page</h1>
+    </DefaultLayout>
+);
+
+export default NotFound;
