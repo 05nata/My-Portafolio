5d224cb02c086cd45787790fb138cb5e8101d050
Natalia Marin Gallego <0502.nataliia@gmail.com>
2022-03-14 21:15:55 -0500

    Define default layout

---
+++ my-app/src/components/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DefaultLayout from '../layout/DefaultLayout';
+
+/**
+ * Home component
+ * @returns {JSX.Element}
+ */
+export const Home = () => <DefaultLayout />
+
+export default Home;
